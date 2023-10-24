5050d3f2bff80f127b4f1cc77c8c473aed7ab335
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-24 17:31:27 +0800

    feat: add ui using vs forms

---
+++ src/app/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace app
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sideNavHeader = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.middleNameLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.birthLabel = new System.Windows.Forms.Label();
+            this.birthPicker = new System.Windows.Forms.DateTimePicker();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.ageText = new System.Windows.Forms.TextBox();
+            this.sexLabel = new System.Windows.Forms.Label();
+            this.sexComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addStudentBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.panel1.Controls.Add(this.sideNavHeader);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 682);
+            this.panel1.TabIndex = 0;
+            // 
+            // sideNavHeader
+            // 
+            this.sideNavHeader.AutoSize = true;
+            this.sideNavHeader.Font = new System.Drawing.Font("Poppins", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sideNavHeader.ForeColor = System.Drawing.Color.White;
+            this.sideNavHeader.Location = new System.Drawing.Point(51, 28);
+            this.sideNavHeader.Name = "sideNavHeader";
+            this.sideNavHeader.Size = new System.Drawing.Size(132, 48);
+            this.sideNavHeader.TabIndex = 2;
+            this.sideNavHeader.Text = "iStudent";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(147)))), ((int)(((byte)(177)))), ((int)(((byte)(166)))));
+            this.panel2.Location = new System.Drawing.Point(24, 79);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 5);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(131)))), ((int)(((byte)(116)))));
+            this.panel4.Controls.Add(this.addStudentBtn);
+            this.panel4.Controls.Add(this.sexComboBox);
+            this.panel4.Controls.Add(this.sexLabel);
+            this.panel4.Controls.Add(this.ageText);
+            this.panel4.Controls.Add(this.ageLabel);
+            this.panel4.Controls.Add(this.birthPicker);
+            this.panel4.Controls.Add(this.birthLabel);
+            this.panel4.Controls.Add(this.emailLabel);
+            this.panel4.Controls.Add(this.middleNameLabel);
+            this.panel4.Controls.Add(this.lastNameLabel);
+            this.panel4.Controls.Add(this.firstNameLabel);
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Controls.Add(this.textBox3);
+            this.panel4.Controls.Add(this.textBox4);
+            this.panel4.Location = new System.Drawing.Point(300, 110);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(722, 539);
+            this.panel4.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(50, 81);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(200, 27);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(50, 316);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 27);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 27);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 27);
+            this.textBox1.TabIndex = 9;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.firstNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.firstNameLabel.ForeColor = System.Drawing.Color.White;
+            this.firstNameLabel.Location = new System.Drawing.Point(50, 53);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(81, 25);
+            this.firstNameLabel.TabIndex = 10;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.lastNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lastNameLabel.ForeColor = System.Drawing.Color.White;
+            this.lastNameLabel.Location = new System.Drawing.Point(50, 136);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(81, 25);
+            this.lastNameLabel.TabIndex = 11;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // middleNameLabel
+            // 
+            this.middleNameLabel.AutoSize = true;
+            this.middleNameLabel.BackColor = System.Drawing.Color.Transparent;
+            this.middleNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.middleNameLabel.ForeColor = System.Drawing.Color.White;
+            this.middleNameLabel.Location = new System.Drawing.Point(50, 211);
+            this.middleNameLabel.Name = "middleNameLabel";
+            this.middleNameLabel.Size = new System.Drawing.Size(167, 25);
+            this.middleNameLabel.TabIndex = 12;
+            this.middleNameLabel.Text = "Middle Name (Optional)";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.BackColor = System.Drawing.Color.Transparent;
+            this.emailLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.emailLabel.ForeColor = System.Drawing.Color.White;
+            this.emailLabel.Location = new System.Drawing.Point(50, 288);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(47, 25);
+            this.emailLabel.TabIndex = 13;
+            this.emailLabel.Text = "Email";
+            // 
+            // birthLabel
+            // 
+            this.birthLabel.AutoSize = true;
+            this.birthLabel.BackColor = System.Drawing.Color.Transparent;
+            this.birthLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.birthLabel.ForeColor = System.Drawing.Color.White;
+            this.birthLabel.Location = new System.Drawing.Point(274, 52);
+            this.birthLabel.Name = "birthLabel";
+            this.birthLabel.Size = new System.Drawing.Size(41, 25);
+            this.birthLabel.TabIndex = 14;
+            this.birthLabel.Text = "Birth";
+            // 
+            // birthPicker
+            // 
+            this.birthPicker.Location = new System.Drawing.Point(274, 80);
+            this.birthPicker.Name = "birthPicker";
+            this.birthPicker.Size = new System.Drawing.Size(254, 27);
+            this.birthPicker.TabIndex = 15;
+            this.birthPicker.Value = new System.DateTime(2023, 10, 25, 23, 59, 59, 0);
+            this.birthPicker.ValueChanged += new System.EventHandler(this.birthPicker_ValueChanged);
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ageLabel.ForeColor = System.Drawing.Color.White;
+            this.ageLabel.Location = new System.Drawing.Point(274, 136);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(38, 25);
+            this.ageLabel.TabIndex = 16;
+            this.ageLabel.Text = "Age";
+            // 
+            // ageText
+            // 
+            this.ageText.Location = new System.Drawing.Point(274, 164);
+            this.ageText.Name = "ageText";
+            this.ageText.Size = new System.Drawing.Size(100, 27);
+            this.ageText.TabIndex = 17;
+            this.ageText.TextChanged += new System.EventHandler(this.ageText_TextChanged);
+            // 
+            // sexLabel
+            // 
+            this.sexLabel.AutoSize = true;
+            this.sexLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sexLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sexLabel.ForeColor = System.Drawing.Color.White;
+            this.sexLabel.Location = new System.Drawing.Point(274, 211);
+            this.sexLabel.Name = "sexLabel";
+            this.sexLabel.Size = new System.Drawing.Size(34, 25);
+            this.sexLabel.TabIndex = 19;
+            this.sexLabel.Text = "Sex";
+            // 
+            // sexComboBox
+            // 
+            this.sexComboBox.FormattingEnabled = true;
+            this.sexComboBox.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.sexComboBox.Location = new System.Drawing.Point(274, 239);
+            this.sexComboBox.Name = "sexComboBox";
+            this.sexComboBox.Size = new System.Drawing.Size(100, 31);
+            this.sexComboBox.TabIndex = 22;
+            this.sexComboBox.SelectedIndexChanged += new System.EventHandler(this.sexComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Poppins", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(294, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 36);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ENTER STUDENT DETAILS";
+            // 
+            // addStudentBtn
+            // 
+            this.addStudentBtn.Location = new System.Drawing.Point(528, 440);
+            this.addStudentBtn.Name = "addStudentBtn";
+            this.addStudentBtn.Size = new System.Drawing.Size(150, 60);
+            this.addStudentBtn.TabIndex = 24;
+            this.addStudentBtn.Text = "+ ADD STUDENT";
+            this.addStudentBtn.UseVisualStyleBackColor = true;
+            this.addStudentBtn.Click += new System.EventHandler(this.addStudentBtn_Click);
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(13)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "dashboard";
+            this.Text = "iStudent";
+            this.Load += new System.EventHandler(this.iStudent_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label sideNavHeader;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label middleNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DateTimePicker birthPicker;
+        private System.Windows.Forms.Label birthLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox ageText;
+        private System.Windows.Forms.Label sexLabel;
+        private System.Windows.Forms.ComboBox sexComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addStudentBtn;
+    }
+}
+
---
+++ src/app/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void iStudent_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void firstNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void lastNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void middleNameText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void emailText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void birthPicker_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ageText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sexComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addStudentBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
