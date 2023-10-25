544c39b3a8cb720a6ab951a0bdba244da5d8d01f
Jam <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-10-25 16:44:43 +0800

    feat: add user interface

---
+++ src/app/AddStudentMessage.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace HICLabActivityOne
+{
+    partial class AddStudentMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.borderPanel = new System.Windows.Forms.Panel();
+            this.confirmBtn = new System.Windows.Forms.Button();
+            this.fName = new System.Windows.Forms.Label();
+            this.studentFirstName = new System.Windows.Forms.Label();
+            this.lName = new System.Windows.Forms.Label();
+            this.studentLastName = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.studentEmail = new System.Windows.Forms.Label();
+            this.birthLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.sexLabel = new System.Windows.Forms.Label();
+            this.studentBirth = new System.Windows.Forms.Label();
+            this.studentAge = new System.Windows.Forms.Label();
+            this.studentSex = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // borderPanel
+            // 
+            this.borderPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.borderPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.borderPanel.Location = new System.Drawing.Point(-3, 409);
+            this.borderPanel.Name = "borderPanel";
+            this.borderPanel.Size = new System.Drawing.Size(712, 36);
+            this.borderPanel.TabIndex = 0;
+            // 
+            // confirmBtn
+            // 
+            this.confirmBtn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.confirmBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(131)))), ((int)(((byte)(116)))));
+            this.confirmBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.confirmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.confirmBtn.Font = new System.Drawing.Font("Poppins SemiBold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmBtn.Location = new System.Drawing.Point(531, 310);
+            this.confirmBtn.Name = "confirmBtn";
+            this.confirmBtn.Size = new System.Drawing.Size(116, 50);
+            this.confirmBtn.TabIndex = 1;
+            this.confirmBtn.Text = "Confirm";
+            this.confirmBtn.UseVisualStyleBackColor = false;
+            this.confirmBtn.Click += new System.EventHandler(this.confirmBtn_Click);
+            // 
+            // fName
+            // 
+            this.fName.AutoSize = true;
+            this.fName.BackColor = System.Drawing.Color.Transparent;
+            this.fName.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fName.Location = new System.Drawing.Point(12, 33);
+            this.fName.Name = "fName";
+            this.fName.Size = new System.Drawing.Size(170, 37);
+            this.fName.TabIndex = 2;
+            this.fName.Text = "First Name      :";
+            // 
+            // studentFirstName
+            // 
+            this.studentFirstName.BackColor = System.Drawing.Color.Transparent;
+            this.studentFirstName.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentFirstName.Location = new System.Drawing.Point(181, 33);
+            this.studentFirstName.Name = "studentFirstName";
+            this.studentFirstName.Size = new System.Drawing.Size(511, 37);
+            this.studentFirstName.TabIndex = 3;
+            this.studentFirstName.Text = "fasfasdf";
+            // 
+            // lName
+            // 
+            this.lName.AutoSize = true;
+            this.lName.BackColor = System.Drawing.Color.Transparent;
+            this.lName.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lName.Location = new System.Drawing.Point(12, 70);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(169, 37);
+            this.lName.TabIndex = 4;
+            this.lName.Text = "Last Name      :";
+            // 
+            // studentLastName
+            // 
+            this.studentLastName.BackColor = System.Drawing.Color.Transparent;
+            this.studentLastName.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentLastName.Location = new System.Drawing.Point(181, 70);
+            this.studentLastName.Name = "studentLastName";
+            this.studentLastName.Size = new System.Drawing.Size(511, 37);
+            this.studentLastName.TabIndex = 5;
+            this.studentLastName.Text = "afdsf";
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.BackColor = System.Drawing.Color.Transparent;
+            this.emailLabel.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailLabel.Location = new System.Drawing.Point(12, 107);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(168, 37);
+            this.emailLabel.TabIndex = 8;
+            this.emailLabel.Text = "Email               :";
+            // 
+            // studentEmail
+            // 
+            this.studentEmail.BackColor = System.Drawing.Color.Transparent;
+            this.studentEmail.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentEmail.Location = new System.Drawing.Point(181, 107);
+            this.studentEmail.Name = "studentEmail";
+            this.studentEmail.Size = new System.Drawing.Size(511, 37);
+            this.studentEmail.TabIndex = 9;
+            this.studentEmail.Text = "afdsaf";
+            // 
+            // birthLabel
+            // 
+            this.birthLabel.AutoSize = true;
+            this.birthLabel.BackColor = System.Drawing.Color.Transparent;
+            this.birthLabel.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.birthLabel.Location = new System.Drawing.Point(12, 144);
+            this.birthLabel.Name = "birthLabel";
+            this.birthLabel.Size = new System.Drawing.Size(170, 37);
+            this.birthLabel.TabIndex = 10;
+            this.birthLabel.Text = "Birth                 :";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ageLabel.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ageLabel.Location = new System.Drawing.Point(12, 181);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(170, 37);
+            this.ageLabel.TabIndex = 11;
+            this.ageLabel.Text = "Age                  :";
+            // 
+            // sexLabel
+            // 
+            this.sexLabel.AutoSize = true;
+            this.sexLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sexLabel.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sexLabel.Location = new System.Drawing.Point(12, 218);
+            this.sexLabel.Name = "sexLabel";
+            this.sexLabel.Size = new System.Drawing.Size(170, 37);
+            this.sexLabel.TabIndex = 12;
+            this.sexLabel.Text = "Sex                   :";
+            // 
+            // studentBirth
+            // 
+            this.studentBirth.BackColor = System.Drawing.Color.Transparent;
+            this.studentBirth.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentBirth.Location = new System.Drawing.Point(181, 144);
+            this.studentBirth.Name = "studentBirth";
+            this.studentBirth.Size = new System.Drawing.Size(511, 37);
+            this.studentBirth.TabIndex = 13;
+            this.studentBirth.Text = "asdfasd";
+            // 
+            // studentAge
+            // 
+            this.studentAge.BackColor = System.Drawing.Color.Transparent;
+            this.studentAge.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentAge.Location = new System.Drawing.Point(181, 181);
+            this.studentAge.Name = "studentAge";
+            this.studentAge.Size = new System.Drawing.Size(511, 37);
+            this.studentAge.TabIndex = 14;
+            this.studentAge.Text = "asfdasf";
+            // 
+            // studentSex
+            // 
+            this.studentSex.BackColor = System.Drawing.Color.Transparent;
+            this.studentSex.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studentSex.Location = new System.Drawing.Point(181, 218);
+            this.studentSex.Name = "studentSex";
+            this.studentSex.Size = new System.Drawing.Size(511, 37);
+            this.studentSex.TabIndex = 15;
+            this.studentSex.Text = "asfasdfs";
+            // 
+            // AddStudentMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(13)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.studentSex);
+            this.Controls.Add(this.sexLabel);
+            this.Controls.Add(this.studentAge);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.studentBirth);
+            this.Controls.Add(this.birthLabel);
+            this.Controls.Add(this.studentEmail);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.studentLastName);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.studentFirstName);
+            this.Controls.Add(this.fName);
+            this.Controls.Add(this.confirmBtn);
+            this.Controls.Add(this.borderPanel);
+            this.Font = new System.Drawing.Font("Poppins", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddStudentMessage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Added Student";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel borderPanel;
+        private System.Windows.Forms.Button confirmBtn;
+        private System.Windows.Forms.Label fName;
+        private System.Windows.Forms.Label studentFirstName;
+        private System.Windows.Forms.Label lName;
+        private System.Windows.Forms.Label studentLastName;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label studentEmail;
+        private System.Windows.Forms.Label birthLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.Label sexLabel;
+        private System.Windows.Forms.Label studentBirth;
+        private System.Windows.Forms.Label studentAge;
+        private System.Windows.Forms.Label studentSex;
+    }
+}
